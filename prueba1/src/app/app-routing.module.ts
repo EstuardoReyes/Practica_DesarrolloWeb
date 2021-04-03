699efaddce63a9c6f7ecce985b5416a65d7535f4
erkest <57339906+erkest@users.noreply.github.com>
2021-04-03 13:22:25 -0600

    Agregados enlaces entre ventanas

--- prueba1/src/app/app-routing.module.ts
+++ prueba1/src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Component, NgModule } from '@angular/core';
+import { ChildrenOutletContexts, RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component'
+import { RegistroComponent } from './components/registro/registro.component'
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'registro',
+    component: RegistroComponent
+  },
+  {
+    path: 'home',
+    component: NavigationBarComponent,
+
+    children: [
+      { path: 'public/add', component: LoginComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- prueba1/src/app/app.module.ts
+++ prueba1/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistroComponent } from './components/registro/registro.component'
+import { PublicationListComponent } from './components/publication-list/publication-list.component';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroComponent,
+    PublicationListComponent,
+    NavigationBarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
