b1ecfa4226403374620cb84cf8ddda150e605d01
canqaavexnim@gmail.com <canqaavexnim@gmail,com>
2021-04-07 23:19:44 -0600

    ventana principal

--- prueba1/src/app/app-routing.module.ts
+++ prueba1/src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PublicationListComponent } from './components/publication-list/publication-list.component';
+import { OlvidarPassComponent } from './components/olvidar-pass/olvidar-pass.component';
+import { Component, NgModule } from '@angular/core';
+import { ChildrenOutletContexts, RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component'
+import { RegistroComponent } from './components/registro/registro.component'
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'registro',
+    component: RegistroComponent
+  },
+  {
+    path: 'contraseña',
+    component: OlvidarPassComponent
+  },
+  {
+    path: 'home',
+    component: NavigationBarComponent
+  },
+  {
+    path: 'publicacion',
+    component: PublicationListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
