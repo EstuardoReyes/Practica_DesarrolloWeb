33f9334350549a49d47117a78956e968503220f1
erkest <57339906+erkest@users.noreply.github.com>
2021-04-11 09:51:46 -0600

    Actualizada tabla de cursos

--- prueba1/src/app/components/perfil-opciones/informacion/informacion.component.ts
+++ prueba1/src/app/components/perfil-opciones/informacion/informacion.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/models/user';
+import { Router } from '@angular/router';
+import { PerfilService } from '../perfil.service';
+
 
 @Component({
   selector: 'app-informacion',
@@ -7,7 +11,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InformacionComponent implements OnInit {
 
-  constructor() { }
+  user: Usuario = {
+    nombre: '',
+    apellido: '',
+    correo: '',
+    pass: ''
+  }
+
+  constructor(private  usuariosSvc: PerfilService, private router: Router) { }
 
   ngOnInit(): void {
   }
